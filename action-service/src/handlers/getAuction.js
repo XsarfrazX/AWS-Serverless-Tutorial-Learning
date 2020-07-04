08f2e6090710dfd5b48c2f0ddfa067e223955eab
XsarfrazX <mohammed.sarfraz@harman.com>
2020-07-04 23:28:35 +0530

    Add common middleware

--- action-service/src/handlers/getAuction.js
+++ action-service/src/handlers/getAuction.js
@@ -1,8 +1,5 @@
 import AWS from 'aws-sdk';
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -28,7 +25,7 @@ async function getAuction(event, context) {
 
 
     if(!auction) {
-        throw new createError.NotFound(`Auction with ID "${id}" Not found`);
+        throw new createError.NotFound(`Auction with ID "${id}" Not found`.toString());
     }
 
 
@@ -38,9 +35,6 @@ async function getAuction(event, context) {
   };
 }
 
-export const handler = middy(getAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(getAuction);
 
 
