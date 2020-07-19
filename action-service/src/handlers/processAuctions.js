1af2d33848c64d70d08da5800623ad68d315bd3b
XsarfrazX <mohammed.sarfraz@harman.com>
2020-07-19 22:53:31 +0530

    Process auctions by closing old auctions >1hr

--- action-service/src/handlers/processAuctions.js
+++ action-service/src/handlers/processAuctions.js
@@ -1,10 +1,20 @@
+import createError from 'http-errors';
 import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
 
 
 async function processAuctions(ecent, context){
 
+    try{
     const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+    await Promise.all(closePromises);
     console.log(auctionsToClose);
+    return { closed: closePromises.length};
+    } catch(error){
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
 
 }
 
